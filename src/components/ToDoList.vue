2d6b108dfaf7f2ea779e93ccb9ea3474872d8ae9
ckthanh-sgt <ckthanh-sgt@gmail.com>
2021-01-18 10:31:02 +0700

    #3 Simple Add - Delete - CLear Form

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -2,8 +2,22 @@
   <v-container id="frmToDo">
     <h3 class="frmToDoTitle">Todo Application</h3>
     <v-form
-      ref="form"
-      lazy-validation>
+      id="frm_ToDo"
+      method="post"
+      action="."
+    >
+      <div id="errors" v-if="errors.length">
+        <ul>
+          <li
+            class="btn btn-danger text-white"
+            v-for="(error, key) in errors"
+            :key="key"
+          >
+            {{ error }}
+          </li>
+        </ul>
+      </div>
+
       <v-container>
         <v-row>
           <v-col cols="12">
@@ -11,7 +25,6 @@
               v-model="todoName"
               :counter="255"
               label="Task name"
-              prend-icon=""
               required
             >
               <i
@@ -33,6 +46,7 @@
           <v-col cols="12">
             <v-btn
               class="v-btn--rounded mr-2"
+              type="button"
               @click="addTodo"
             >
               Add
@@ -50,40 +64,66 @@
     <table class="table" id="listTodoes">
       <thead>
       <tr>
+        <th>
+          <input type="checkbox" @click="checkAll(true)">
+        </th>
         <th scope="col">Order</th>
         <th scope="col">Task Name</th>
         <th scope="col">Date</th>
+        <th scope="col">Status</th>
         <th scope="col">Actions</th>
       </tr>
       </thead>
       <tbody>
       <tr
-        v-for="todo in todoes"
-        v-if="todo.status"
-        v-bind:key="todo.id"
+        v-for="(todo, key) in todoes"
+        :key="key"
+        :class="{ 'done-task' : todo.completed }"
       >
-        <th scope="row">{{todo.id}}</th>
-        <td>{{todo.todoName}}</td>
-        <td>{{todo.created_at}}</td>
+        <th>
+          <input type="checkbox" value="" class="">
+          <v-input
+            type="checkbox"
+            :checked="todo.checked"
+          ></v-input>
+        </th>
+        <th>{{ todo.id }}</th>
+        <td>{{ todo.todoName }}</td>
+        <td>{{ todo.created_at }}</td>
         <td>
-          <button
-            @click="editTodo"
-            type="button"
-            aria-label="Edit"
-            title="Edit"
-            class="btn btn-primary text-white"
-          >
-            Edit
-          </button>
-          <button
-            @click="removeTodo"
-            type="button"
-            aria-label="Delete"
-            title="Delete"
-            class="btn btn-danger text-white"
-          >
-            Delete
-          </button>
+          {{ todo.status === 1 ? 'Done' : 'Doing' }}
+        </td>
+        <td>
+          <div v-if="!todo.completed">
+            <button
+              @click="editTodo"
+              type="button"
+              aria-label="Edit"
+              title="Edit"
+              class="btn btn-primary text-white"
+            >
+              Edit
+            </button>
+            <button
+              @click="removeTodo(key)"
+              type="button"
+              aria-label="Delete"
+              title="Delete"
+              class="btn btn-danger text-white"
+            >
+              Delete
+            </button>
+          </div>
+          <div v-else>
+            <button
+              @click="viewTodo(todo.id)"
+              type="button"
+              title="Undo"
+              class="btn btn-warning text-white"
+            >
+              View
+            </button>
+          </div>
         </td>
       </tr>
       </tbody>
@@ -92,13 +132,22 @@
 </template>
 
 <script>
+import moment from 'moment'
 
 export default {
   name: 'ToDoList',
+  beforeCreate () {
+    console.log('Before Create Function: Nothing starts before me!')
+  },
   data: () => ({
     search: '',
+    errors: [],
+    deleted_at: '',
     todoName: '',
     todoDescription: '',
+    status: '',
+    completed: '',
+    checked: false,
     todoes: [
       {
         'id': 1,
@@ -107,7 +156,9 @@ export default {
         'deleted_at': '',
         'todoName': 'Meeting with customer at 18:00pm',
         'todoDescription': 'Have an appointment with VIP Customer at 18:00pm',
-        'status': 1
+        'status': 0,
+        'completed': 0,
+        'checked': false
       },
       {
         'id': 2,
@@ -116,7 +167,9 @@ export default {
         'deleted_at': '',
         'todoName': 'Meeting with customer at 20:00pm',
         'todoDescription': 'Have an appointment with VIP Customer at 20:00pm',
-        'status': 1
+        'status': 1,
+        'completed': 1,
+        'checked': false
       },
       {
         'id': 3,
@@ -125,24 +178,81 @@ export default {
         'deleted_at': '',
         'todoName': 'Meeting with customer at 22:00pm',
         'todoDescription': 'Have an appointment with VIP Customer at 22:00pm',
-        'status': 1
+        'status': 1,
+        'completed': 1,
+        'checked': false
       }
     ]
   }),
   methods: {
     addTodo: function () {
-      alert('Add todo')
+      // Check validation
+      if (this.todoName.trim().length === 0) {
+        this.errors.push('Please enter task name')
+        return
+      }
+
+      if (this.todoDescription.trim().length === 0) {
+        this.errors.push('Please enter some descriptions')
+        return
+      }
+
+      // Add data
+      this.todoes.push({
+        'id': this.todoes.length + 1,
+        'created_at': moment().format('YYYY-MM-DD'),
+        'updated_at': '',
+        'deleted_at': '',
+        'todoName': this.todoName,
+        'todoDescription': this.todoDescription,
+        'status': 1,
+        'completed': 0,
+        'checked': false
+      })
+
+      // Clear all data after success
+      while (this.errors.length > 0) {
+        this.errors.pop()
+      }
+      // Clear input data after success
+      this.$data.todoName = ''
+      this.$data.todoDescription = ''
     },
     editTodo: function () {
       alert('Edit todo')
     },
-    removeTodo: function () {
-      alert('Are you sure to delete?')
+    removeTodo: function (key) {
+      if (confirm('Are you sure to delete?')) {
+        // Method One:
+        // this.$delete(this.todoes, key)
+
+        // Method Two:
+        this.todoes.splice(key, 1)
+      }
     },
-    clearForm: function () {
-      alert('Clear form data')
+    clearForm: function (e) {
+      e.preventDefault()
+      this.$data.todoName = ' '
+      this.$data.todoDescription = ' '
+      this.errors = []
+    },
+    checkAll: function (flag) {
+      alert('Check all' + flag)
+      this.todoes.forEach(todo => {
+        todo.checked = flag
+      })
+    },
+    viewTodo: function (key) {
+      alert('View task: ' + key)
+    }
+  },
+  computed: {
+    // Get total of tasks
+    totalTasks: function () {
+      return this.totalTasks + ' ' + this.todoDescription
     }
   }
+
 }
 </script>
 
@@ -152,9 +262,9 @@ export default {
   background: white;
   position: absolute;
   min-height: 800px;
-  max-width: 800px;
+  max-width: 1100px;
   top: calc(50% - 450px);
-  left: calc(50% - 400px);
+  left: calc(50% - 550px);
   padding: 30px 20px;
 }
 
@@ -172,5 +282,20 @@ h3.frmToDoTitle {
 
 table#listTodoes {
   margin-top: 32px;
+}
+
+.done-task {
+  color: darkorange;
+  text-decoration: line-through;
+}
+
+#errors ul {
+  padding-left: 11px;
+  list-style: none;
+  margin-top: 20px;
+}
+
+#errors ul li {
+  margin-right: 12px;
 }
 </style>
